5f7f684a4724c3fb0dd40d969fa7f6e4200b8c6c
nihano <nioztekin@gmail.com>
2022-10-10 01:04:26 +0300

    adds validation

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -6,10 +6,13 @@ import com.cydeo.model.Employee;
 import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -32,10 +35,14 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee ){
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model){ //added @Valid to make controller be aware of the validations in Employee class
+        //you need to add BindingResult parameter after the object
 
+        if (bindingResult.hasErrors()){ //if there is any error in Employee obj fileds related with validation
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
         employeeService.saveEmployee(employee);
-
         return "redirect:/employee/list"; //it will redirect this to below method (page as a result)
         //with redirect we are using endpoints
     }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,7 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import javax.websocket.server.ServerEndpoint;
 import java.time.LocalDate;
 
@@ -13,14 +14,34 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+
+//    @NotNull      ----> Field shouldn't be null
+//    @NotEmpty     ----> Field shouldn't be ""
+//    @NotBlank     ----> Field shouldn't be "   "
+
+//    @NotNull -> @NotNull
+//    @NotEmpty ->  @NotNull + @NotEmpty
+//    @NotBlank ->  @NotNull + @NotEmpty + @NotBlank
+    @NotBlank
+    @Size(max=12, min = 2)
     private String firstName;
+
     private String lastName;
 
     //thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy or yyyy-dd-mm or sth else
+
+//    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
+//    @NotBlank
+//    @Email
     private String email;
+
+//    @NotBlank
+//    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}") //google this kind of conditions for passwords regexp
     private String password;
+
     private String address;
     private String address2;
     private String city;
