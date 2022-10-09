5b947765f462e4d4785880d63a7661f604a8792d
nihano <nioztekin@gmail.com>
2022-10-09 23:29:25 +0300

    added method for posting

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -3,14 +3,23 @@ package com.cydeo.controller;
 
 import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    private final EmployeeService employeeService; //for dependency injection
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/register")
     public String createEmployee(Model model){
@@ -20,5 +29,21 @@ public class EmployeeController {
 
         return "/employee/employee-create";
 
+    }
+
+    @PostMapping("/insert")
+    public String insertEmployee(@ModelAttribute("employee") Employee employee ){
+
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/employee/list"; //it will redirect this to below method (page as a result)
+        //with redirect we are using endpoints
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model){
+        model.addAttribute("employeeList", employeeService.readAllEmployees());
+
+        return "employee/employee-list"; //without redirect we are usuing html file paths
     }
 }
