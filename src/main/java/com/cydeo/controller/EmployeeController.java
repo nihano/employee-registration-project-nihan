efe87ad59fe0466411caef37644fd626d7a5ccee
nihano <nioztekin@gmail.com>
2022-10-09 22:24:17 +0300

    adds employee and stateList models in form

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "/employee/employee-create";
 
